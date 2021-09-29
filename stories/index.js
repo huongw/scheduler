450a589f4ec31c083383c2b1f7f740d1ef2bc600
WH <whitneyhuong+gitkraken@gmail.com>
2021-09-29 17:17:19 -0400

    complete error component and fix interviewer name in story for show component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,8 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 // Storybook
 import { storiesOf } from "@storybook/react";
@@ -134,7 +136,7 @@ storiesOf("Appointment", module)
   .add("Show", () => (
     <Show
       student={"Lydia Miller-Jones"}
-      interviewer={1}
+      interviewer={interviewer.name}
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
     />
@@ -144,5 +146,12 @@ storiesOf("Appointment", module)
       message={"Delete the appointment?"}
       onConfirm={action("onConfirm")}
       onCancel={action("onCancel")}
+    />
+  ))
+  .add("Status", () => <Status message={"Deleting"} />)
+  .add("Error", () => (
+    <Error
+      message={"Could not delete appointment."}
+      onClose={action("onClose")}
     />
   ))
