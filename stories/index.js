9c17f4138a15e6b48c7026bb86a4bb084179f48c
WH <whitneyhuong+gitkraken@gmail.com>
2021-09-29 16:03:24 -0400

    finish stretch activity

--- stories/index.js
+++ stories/index.js
@@ -97,13 +97,13 @@ storiesOf("InterviewerList", module)
   .add("Initial", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ))
   .add("Preselected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
-      setInterviewer={action("setInterviewer")}
+      value={3}
+      onChange={action("setInterviewer")}
     />
   ));
