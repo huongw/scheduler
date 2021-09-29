c1268dff210c5d592ce77cbb46ea1ff107a0d933
WH <whitneyhuong+gitkraken@gmail.com>
2021-09-29 15:45:03 -0400

    update index from stories and add key/id to interviewerlistitem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,14 +3,14 @@ import classNames from "classnames";
 import React from "react";
 
 export default function InterviewerListItem(props) {
-  const { name, avatar, setInterviewer, selected } = props;
+  const { id, name, avatar, setInterviewer, selected } = props;
 
   const interviewerClass = classNames("interviewers__item", {
     "interviewers__item--selected": selected
   });
 
   return (
-    <li className={interviewerClass} onClick={() => setInterviewer(name)}>
+    <li className={interviewerClass} onClick={setInterviewer}>
       <img
         className="interviewers__item-image"
         src={avatar}
