c5312a72ab59d73ad67e8de5b15dd56e0a19adad
WH <whitneyhuong+gitkraken@gmail.com>
2021-09-29 13:24:19 -0400

    complete buttons and daylistitem/ daylist files

---
+++ src/components/DayListItem.js
@@ -0,0 +1,33 @@
+import "components/DayListItem.scss";
+import classNames from "classnames";
+import React from "react";
+
+
+
+export default function DayListItem(props) {
+  const { name, spots, selected, setDay } = props;
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": selected,
+    "day-list__item--full": !spots
+  });
+
+  function formatSpots() {
+    if (!spots) {
+      return "no spots remaining";
+    }
+
+    if (spots === 1) {
+      return "1 spot remaining";
+    }
+
+    return `${spots} spots remaining`;
+  }
+
+  return (
+    <li onClick={() => setDay(name)} className={dayClass}>
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
@@ -21,4 +22,26 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+storiesOf("DayListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />)
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />)
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
+  ));
+
+storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
   ));
