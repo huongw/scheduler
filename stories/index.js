10eb60bd8d543ccf21d3dd66e286f432bb13e193
WH <whitneyhuong+gitkraken@gmail.com>
2021-09-29 19:46:28 -0400

    create form component and update story

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 // Storybook
 import { storiesOf } from "@storybook/react";
@@ -153,5 +154,21 @@ storiesOf("Appointment", module)
     <Error
       message={"Could not delete appointment."}
       onClose={action("onClose")}
+    />
+  ))
+  .add("Edit", () => (
+    <Form
+      name={""}
+      interviewers={interviewers}
+      interviewer={3}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
