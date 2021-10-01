6b97a720876a07db504ba4d458a2d26b17b69ee2
WH <whitneyhuong+gitkraken@gmail.com>
2021-10-01 17:37:05 -0400

    ran unit tests for getAppointmentsForDay

---
+++ src/helpers/selectors.js
@@ -0,0 +1,18 @@
+export function getAppointmentsForDay(state, day) {
+
+  const dayObj = state.days.find((obj) => {
+    return obj.name === day;
+  })
+
+  if (!dayObj || state.days.length === 0) {
+    return [];
+  }
+
+  const appointmentIds = dayObj.appointments;
+
+  const appointments = appointmentIds.map((id) => {
+    return state.appointments[id];
+  })
+
+  return appointments;
+};
