7262962099c7f182c4ede118fda5b188fa472650
WH <whitneyhuong+gitkraken@gmail.com>
2021-10-04 15:56:21 -0400

    refactor useVisualMode and update Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,38 @@
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import "components/Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
 import React from "react";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { time, interview } = props;
 
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
+  const onAdd = function () {
+    transition(CREATE)
+  }
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={onAdd} />}
+      {mode === SHOW && (
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
+
     </article>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,26 +1,27 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
+
   function transition(mode, replace = false) {
-    
-    if (replace) {
-      setMode(mode)
-    } else {
-      setMode(mode)
-      history.push(mode)
-      setHistory(history)
+
+    if (!replace) {
+      const updatedHistory = [...history, mode]
+      setHistory(updatedHistory)
     }
+
+    setMode(mode)
   }
 
   function back() {
     if (history.length > 1) {
-    history.pop()
-    setMode(history[history.length-1])
+      const updatedHistory = history.slice(0, history.length - 1)
+      setMode(updatedHistory[updatedHistory.length - 1])
+      setHistory(updatedHistory)
     }
   }
 
-  return {mode, transition, back};
+  return { mode, transition, back };
 }
